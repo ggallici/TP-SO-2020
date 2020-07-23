cb7d53df49f2b990dd1bdf3a5f9beead27526859
ggallici <gallicig94@gmail.com>
2020-07-23 16:33:26 -0300

    termino las funciones que faltaban y las comento

--- broker/src/logger.c
+++ broker/src/logger.c
@@ -7,27 +7,36 @@
 
 #include "logger.h"
 
-int list_sum_by(t_list * lista, int (*transformador)(void*)) {
-    int _sumar_aplicando_transformador(int acumulador, void* elemento) {
-        return acumulador + transformador(elemento);
-    }
-
-    return (int) list_fold(lista, (void*) 0, (void*) _sumar_aplicando_transformador);
-}
 
 
-char* get_particiones_serializadas(t_list* particiones) {
-//    char* _sasa_aplicando_transformador(char* acumulador, t_particion* particion) {
-//        char* particion_string = string_from_format("[%s=%i] ", particion->esta_libre ? "L" : "X", particion->tamanio);
-//        char* resultado = string_append(acumulador, particion_string);
-//        free(particion_string);
-//
-//        return resultado;
-//    }
-//
-//    return list_fold(particiones, (void*) string_new(), (void*) _sasa_aplicando_transformador);
-    return NULL;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -36,20 +45,52 @@ char* get_particiones_serializadas(t_list* particiones) {
 
 
 /*
-//BOLUDECES
-Inicialización del Broker
-Comienzo de hilos despachantes
-Comienzo de hilo que escucha
 
-//COMPLICADOS O QUE DAN PAJA
-Memoria libre y ocupada ⇒ después de eliminar y después de almacenar (debugg) y tmb para consolidacion y compactacion (NO SE SI ANTES O DESPUÉS)
-Memoria libre = 0 ; Memoria ocupada = 64
-[LI=50] [OC=40] [LI=12] [LI=10]
- */
+NO TOCAR: ANDA Y PAJA HACERLE TEST
 
+int list_sum_by(t_list* lista, int (*key_extractor)(void*)) {
+    int _sum_with_extractor(int acumulador, void* elemento) {
+        return acumulador + key_extractor(elemento);
+    }
 
+    return (int) list_fold(lista, (void*) 0, (void*) _sum_with_extractor);
+}
 
+int _tamanio_particion_si_esta_libre_sino_0(t_particion* particion) {
+    return particion->esta_libre ? particion->tamanio : 0;
+}
+int _tamanio_particion_si_esta_ocupada_sino_0(t_particion* particion) {
+    return !particion->esta_libre ? particion->tamanio : 0;
+}
 
+log_debug(logger_debug, "Memoria libre: %i", list_sum_by(buzon->memoria->particiones, _tamanio_particion_si_esta_libre_sino_0));
+log_debug(logger_debug, "Memoria ocupada: %i", list_sum_by(buzon->memoria->particiones, _tamanio_particion_si_esta_ocupada_sino_0));
 
+*/
 
+/*
 
+NO TOCAR: ANDA Y PAJA HACERLE TEST
+
+char* get_particiones_as_string(t_list* particiones) {
+    char* _concat_with_particion(char* acumulador, t_particion* particion) {
+        char* detalle_particion = string_from_format("[%s=%i] ", particion->esta_libre ? "L" : "X", particion->tamanio);
+        string_append(&acumulador, detalle_particion);
+        free(detalle_particion);
+
+        return acumulador;
+    }
+
+    return list_fold(particiones, (void*) string_new(), (void*) _concat_with_particion);
+}
+
+log_debug(logger_debug, "Detalle particiones: %s", get_detalle_particiones(buzon->memoria->particiones));
+
+*/
+
+/*
+    BOLUDECES
+    * Inicialización del Broker
+    * Comienzo de hilos despachantes
+    * Comienzo de hilo que escucha
+*/
